afdb5986978614ac233a05400b371c70ce95da89
eisen.aviv@gmail.com <Nemesis23321>
2014-07-01 23:08:23 +0300

    Adding verbosity to logger

--- MISC/logger.py
+++ MISC/logger.py
@@ -2,8 +2,12 @@ __author__ = 'aviv'
 
 import os
 import sys
+import datetime
 
 from singleton import Singleton
+from enum import enum
+
+Verbosity = enum(QUITE=1, VERBOSE=2, DEBUG=3)
 
 class Logger(object):
 
@@ -12,10 +16,14 @@ class Logger(object):
     def __init__(self):
         output_file_name = 'double_encoder_' + str(datetime.datetime.now()) + '.txt'
         self.output_file = open(output_file_name, 'w+')
+        self.verbosity = Verbosity.QUITE
 
-    def write(self, message):
-        self.output_file.write(message)
+    def set_verbosity(self, verbosity):
+        self.verbosity = verbosity
 
+    def write(self, message, verbosity):
+        if verbosity >= self.verbosity:
+            self.output_file.write(message)
 
     def __del__(self):
         self.output_file.close()
