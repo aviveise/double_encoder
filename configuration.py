af3ef15975d5a2afe69ca87846ab709db6ab39bb
eisen.aviv@gmail.com <Nemesis23321>
2014-07-02 23:47:26 +0300

    Creating configuration class

---
+++ configuration.py
@@ -0,0 +1,39 @@
+import ConfigParser
+
+from MISC.utils import ConfigSectionMap
+from hyper_parameters import HyperParameters
+
+__author__ = 'Aviv Eisenschtat'
+
+class Configuration(object):
+
+    def __init__(self, config_file_path):
+
+        config = ConfigParser.ConfigParser()
+        config.read(config_file_path)
+
+        self.optimizations_parameters = []
+
+        sections = config.sections()
+        for section in sections:
+
+            if section.startswith('optimization_'):
+                self.optimizations.append(ConfigSectionMap(section, config))
+
+            elif section.startswith('regularization_'):
+                self.regularization.append(ConfigSectionMap(section, config))
+
+        self.hyper_parameters = self._parse_training_parameters(ConfigSectionMap("Hyper_parameters", config))
+
+    def _parse_training_parameters(self, training_section):
+        learning_rate = float(training_section['learning_rate'])
+        batch_size = int(training_section['batch_size'])
+        epochs = int(training_section['epochs'])
+        momentum = float(training_section['momentum'])
+        layer_sizes = map(int, training_section['layer_sizes'].split())
+
+        return HyperParameters(layer_sizes=layer_sizes,
+                               learning_rate=learning_rate,
+                               batch_size=batch_size,
+                               epochs=epochs,
+                               momentum=momentum)
