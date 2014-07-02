46549e017839763634dc7a042012a8c98cf4b156
eisen.aviv@gmail.com <Nemesis23321>
2014-07-02 23:30:13 +0300

    Removing regularization from hyper parameters

--- hyper_parameters.py
+++ hyper_parameters.py
@@ -9,17 +9,13 @@ class HyperParameters(object):
                        learning_rate = 0,
                        batch_size = 0,
                        epochs = 0,
-                       momentum = 0,
-                       regularization_type = None,
-                       regularization_parameters = None):
+                       momentum = 0):
 
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.epochs = epochs
         self.momentum = momentum
         self.layer_sizes = layer_sizes
-        self.regularization_type = regularization_type
-        self.regularization_parameters = regularization_parameters
 
     def copy(self):
 
@@ -27,9 +23,7 @@ class HyperParameters(object):
                                self.learning_rate,
                                self.batch_size,
                                self.epochs,
-                               self.momentum,
-                               self.regularization_type,
-                               self.regularization_parameters)
+                               self.momentum)
 
     def print_parameters(self, output_stream):
 
@@ -37,13 +31,11 @@ class HyperParameters(object):
         output_stream.write('learning_rate: %f\n'
                             'batch_size: %d\n'
                             'epochs: %d\n'
-                            'Momentum: %f\n'
-                            'Regularization type: %s\n'
-                            'Regularization weight: %f\n' % (self.learning_rate,
-                                                             self.batch_size,
-                                                             self.epochs,
-                                                             self.momentum,
-                                                             self.regularization_type,
-                                                             self.regularization_parameters['weight']))
+                            'Momentum: %f\n' % (self.learning_rate,
+                                                self.batch_size,
+                                                self.epochs,
+                                                self.momentum,
+                                                self.regularization_type,
+                                                self.regularization_parameters['weight']))
 
         output_stream.flush()
