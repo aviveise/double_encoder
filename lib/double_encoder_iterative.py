dbf25f4697a626a7fbe9bbf20f309a82f202151c
eisen.aviv@gmail.com <Nemesis23321>
2014-08-30 00:02:45 +0300

    reorginzing dirs

--- lib/double_encoder_iterative.py
+++ lib/double_encoder_iterative.py
@@ -1,5 +1,5 @@
-from lib.double_encoder import DoubleEncoder
-from lib.training_strategy.iterative_training_strategy import IterativeTrainingStrategy
+from double_encoder import DoubleEncoder
+from training_strategy.iterative_training_strategy import IterativeTrainingStrategy
 
 __author__ = 'aviv'
 
