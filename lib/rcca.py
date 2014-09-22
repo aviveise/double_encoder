972b3aa46148ba6e2515334a5d598699676136b7
eisen.aviv@gmail.com <Nemesis23321>
2014-09-22 21:55:24 +0300

    fixing iterative strategy

--- lib/rcca.py
+++ lib/rcca.py
@@ -11,18 +11,13 @@ from MISC.utils import ConfigSectionMap
 
 __author__ = 'aviv'
 
-def GetMatrix(data):
-
-    matrix = robjects.r['matrix'](0, data.shape[0], data.shape[1])
-
-    for i in xrange(data.shape[0]):
-        floatVector = robjects.FloatVector(data_set.trainset[0][1, :])
-        matrix.rx[i, :] = floatVector
-
 if __name__ == '__main__':
 
     data_set_config = sys.argv[1]
     rca_location = sys.argv[2]
+    kx = sys.argv[3]
+    ky = sys.argv[4]
+    top = sys.argv[5]
 
     data_config = ConfigParser.ConfigParser()
     data_config.read(data_set_config)
@@ -37,9 +32,9 @@ if __name__ == '__main__':
 
     rcca_fit = robjects.r('rcca_fit')(x=numpy2ri.numpy2ri(data_set.trainset[0].T),
                                       y=numpy2ri.numpy2ri(data_set.trainset[1].T),
-                                      kx=data_set.trainset[0].shape[0],
-                                      ky=data_set.trainset[1].shape[0],
-                                      top=112,
+                                      kx=kx,
+                                      ky=ky,
+                                      top=top,
                                       type='nystrom')
 
     print 'evaluating rcca'
