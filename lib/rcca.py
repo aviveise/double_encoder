45d37860cfc7b11be8f337d69cf5b0887837e749
eisen.aviv@gmail.com <Nemesis23321>
2014-10-10 10:37:08 +0300

    fix in rcca

--- lib/rcca.py
+++ lib/rcca.py
@@ -6,6 +6,7 @@ import rpy2.robjects.numpy2ri as numpy2ri
 import rpy2.robjects as robjects
 import DataSetReaders
 import numpy
+import matplotlib.pyplot as plt
 
 from Testers.trace_correlation_tester import TraceCorrelationTester
 from Transformers.identity_transform import IdentityTransformer
@@ -31,10 +32,26 @@ if __name__ == '__main__':
 
     robjects.r('source("%s")' % rca_location)
 
+    mnist = robjects.r('load')('/home/aviv/Project/DoubleEncoder/DataSet/MNIST_SPLIT/mnist.data')
+
+    x1 = numpy.array(robjects.r['x_tr'])
+    x2 = numpy.array(robjects.r['y_tr'])
+
+    x1_test = numpy.array(robjects.r['x_te'])
+    x2_test = numpy.array(robjects.r['y_te'])
+
+    plt.imshow(x1_test.T[:, 1].reshape([14, 28]))
+    plt.gray()
+    plt.show()
+
+    plt.imshow(x2_test.T[:, 1].reshape([14, 28]))
+    plt.gray()
+    plt.show()
+
     print 'training rcca'
 
-    rcca_fit = robjects.r('rcca_fit')(x=numpy2ri.numpy2ri(data_set.trainset[0].T),
-                                      y=numpy2ri.numpy2ri(data_set.trainset[1].T),
+    rcca_fit = robjects.r('rcca_fit')(x=numpy2ri.numpy2ri(data_set.trainset[1].T),
+                                      y=numpy2ri.numpy2ri(data_set.trainset[0].T),
                                       kx=kx,
                                       ky=ky,
                                       top=top,
@@ -43,8 +60,8 @@ if __name__ == '__main__':
     print 'evaluating rcca'
 
     rcca_eval = robjects.r('rcca_eval')(rcca=rcca_fit,
-                                        x=numpy2ri.numpy2ri(data_set.testset[0].T),
-                                        y=numpy2ri.numpy2ri(data_set.testset[1].T))
+                                        x=numpy2ri.numpy2ri(data_set.testset[1].T),
+                                        y=numpy2ri.numpy2ri(data_set.testset[0].T))
 
     x = numpy.array(rcca_eval[0])
     y = numpy.array(rcca_eval[1])
