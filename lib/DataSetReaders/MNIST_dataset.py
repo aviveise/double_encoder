6af355e3b15148211e8867e3543b6c80c2d7771e
eisen.aviv@gmail.com <Nemesis23321>
2014-10-10 13:25:30 +0300

    loading mnist from rcca

--- lib/DataSetReaders/MNIST_dataset.py
+++ lib/DataSetReaders/MNIST_dataset.py
@@ -5,6 +5,9 @@ import theano
 from MISC.container import ContainerRegisterMetaClass
 from dataset_base import DatasetBase
 
+import numpy
+import rpy2.robjects as robjects
+
 class MNISTDataSet(DatasetBase):
 
     __metaclass__ = ContainerRegisterMetaClass
@@ -30,5 +33,23 @@ class MNISTDataSet(DatasetBase):
         self.trainset = x1_train_set, x2_train_set
         self.tuning = x1_tuning_set, x2_tuning_set
 
+class MNISTDataSetRCCA(DatasetBase):
 
+    __metaclass__ = ContainerRegisterMetaClass
 
+    def __init__(self, data_set_parameters):
+        super(MNISTDataSet, self).__init__(data_set_parameters)
+
+    def build_dataset(self):
+
+        mnist = robjects.r('load')(self.dataset_path)
+
+        self.trainset = numpy.array(robjects.r['x_tr']), numpy.array(robjects.r['y_tr'])
+
+        self.testset = numpy.array(robjects.r['x_te']), numpy.array(robjects.r['y_te'])
+
+        x1_train_set, x1_tuning_set, test_samples = self.produce_optimization_sets(self.trainset[0])
+        x2_train_set, x2_tuning_set, test_samples = self.produce_optimization_sets(self.trainset[1], test_samples)
+
+        self.trainset = x1_train_set, x2_train_set
+        self.tuning = x1_tuning_set, x2_tuning_set
