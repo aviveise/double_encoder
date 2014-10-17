2b54901cdf71c529978a3fa3bc2613391064a37e
eisen.aviv@gmail.com <Nemesis23321>
2014-10-17 13:49:00 +0300

    reducing variance regularization

--- lib/Regularizations/pairwise_correlation_regularization.py
+++ lib/Regularizations/pairwise_correlation_regularization.py
@@ -40,8 +40,8 @@ class PairWiseCorrelationRegularization(RegularizationBase):
                 print 'added pair reg'
 
             if self.variance:
-                regularization -= Tensor.mean(Tensor.dot(forward_centered, forward_centered.T))
-                regularization -= Tensor.mean(Tensor.dot(backward_centered, backward_centered.T))
+                regularization -= Tensor.mean(Tensor.dot(forward_centered, forward_centered.T) ** 2)
+                regularization -= Tensor.mean(Tensor.dot(backward_centered, backward_centered.T) ** 2)
                 print 'added var reg'
 
 
