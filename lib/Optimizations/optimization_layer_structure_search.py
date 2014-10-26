83e45079e643b7d4ae3b4d06d0d775675eec0fae
eisen.aviv@gmail.com <Nemesis23321>
2014-10-26 19:36:32 +0200

    exception fix

--- lib/Optimizations/optimization_layer_structure_search.py
+++ lib/Optimizations/optimization_layer_structure_search.py
@@ -17,7 +17,8 @@ class OptimizationLayerStructureSearch(OptimizationBase):
         self.start_value = int(optimization_parameters['start_value'])
         self.end_value = int(optimization_parameters['end_value'])
         self.rounds_number = int(optimization_parameters['rounds_number'])
-        self.layer_number = int(optimization_parameters['hidden_layer_number'])
+        self.layer_number_end = int(optimization_parameters['hidden_layer_number_end'])
+        self.layer_number_start = int(optimization_parameters['hidden_layer_number_start'])
 
     def perform_optimization(self, training_strategy):
 
@@ -26,40 +27,42 @@ class OptimizationLayerStructureSearch(OptimizationBase):
 
         hyper_parameters = self.hyper_parameters.copy()
         random_rng = RandomState()
-        hyper_parameters.layer_sizes = random_rng.uniform(self.start_value,
-                                                          self.end_value,
-                                                          self.layer_number)
 
         improvement_rounds = 0
         best_correlation = 0
         previous_correlation = 0
+        for layer_num in xrange(int(self.layer_number_start),int(self.layer_number_end)):
 
-        for iteration in xrange(int(self.rounds_number)):
+            hyper_parameters.layer_sizes = random_rng.uniform(self.start_value,
+                                                          self.end_value,
+                                                          layer_size)
 
-            hyper_parameters.layer_sizes = [int(round(layer_size)) for layer_size in hyper_parameters.layer_sizes]
+            for iteration in xrange(int(self.rounds_number)):
 
-            correlation, execution_time = self.train(training_strategy=training_strategy, hyper_parameters=hyper_parameters)
+                hyper_parameters.layer_sizes = [int(round(layer_size)) for layer_size in hyper_parameters.layer_sizes]
 
-            if correlation < previous_correlation:
-                improvement_rounds = 0
-                previous_correlation = correlation
-            else:
-                improvement_rounds += 1
+                correlation, execution_time = self.train(training_strategy=training_strategy, hyper_parameters=hyper_parameters)
 
-            if improvement_rounds > 0:
-                hyper_parameters.layer_sizes = [layer_size + 50 for layer_size in hyper_parameters.layer_sizes]
+                if correlation < previous_correlation:
+                    improvement_rounds = 0
+                    previous_correlation = correlation
+                else:
+                    improvement_rounds += 1
 
-            else:
-                hyper_parameters.layer_sizes = hyper_parameters.layer_sizes = random_rng.uniform(self.start_value,
-                                                                                                 self.end_value,
-                                                                                                 self.layer_number)
-            if correlation > best_correlation:
-                best_correlation = correlation
-                self.hyper_parameters.layer_sizes = hyper_parameters.layer_sizes
+                if improvement_rounds > 0:
+                    hyper_parameters.layer_sizes = [layer_size + 50 for layer_size in hyper_parameters.layer_sizes]
 
-            OutputLog().write('%s, %f, %f\n' % (print_list(hyper_parameters.layer_sizes),
-                                                correlation,
-                                                execution_time))
+                else:
+                    hyper_parameters.layer_sizes = hyper_parameters.layer_sizes = random_rng.uniform(self.start_value,
+                                                                                                     self.end_value,
+                                                                                                     layer_num)
+                if correlation > best_correlation:
+                    best_correlation = correlation
+                    self.hyper_parameters.layer_sizes = hyper_parameters.layer_sizes
+
+                OutputLog().write('%s, %f, %f\n' % (print_list(hyper_parameters.layer_sizes),
+                                                    correlation,
+                                                    execution_time))
 
         OutputLog().write('----------------------------------------------------------')
 
--- lib/Regularizations/pairwise_correlation_regularization.py
+++ lib/Regularizations/pairwise_correlation_regularization.py
@@ -84,9 +84,6 @@ class PairWiseCorrelationRegularization(RegularizationBase):
         result = scan(lambda eig, eigU:{k:k + Tensor.sqrt(eig) * Tensor.dot(eigU.reshape([n, 1]), eigU.reshape([1, n]))},
                         sequences=[w, v.T])
 
-        #for i in xrange(n):
-        #    k += Tensor.sqrt(w[i]) + Tensor.dot(v[:, i].reshape([n, 1]), v[:, i].reshape([1, n]))
-
         return k
 
 
