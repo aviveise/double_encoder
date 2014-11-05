960f9f060c9551b7ae92d01f0d92eda4d823decf
eisen.aviv@gmail.com <Nemesis23321>
2014-11-05 21:34:43 +0200

    adding imports

--- lib/Testers/trace_correlation_tester.py
+++ lib/Testers/trace_correlation_tester.py
@@ -41,8 +41,8 @@ class TraceCorrelationTester(TesterBase):
         print 'corrcoef = %f\n' % calculate_corrcoef(x, y, self.top)
         sys.stdout.flush()
 
-        print 'mardia = %f\n' % calculate_mardia(x, y, self.top)
-        sys.stdout.flush()
+        #print 'mardia = %f\n' % calculate_mardia(x, y, self.top)
+        #sys.stdout.flush()
 
         return result
 
