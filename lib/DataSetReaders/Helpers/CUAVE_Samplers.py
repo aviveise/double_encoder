725f85b45084d739b8bcb2047db4da1de0796cff
eisen.aviv@gmail.com <Nemesis23321>
2015-03-23 15:37:22 +0200

    sdsd

--- lib/DataSetReaders/Helpers/CUAVE_Samplers.py
+++ lib/DataSetReaders/Helpers/CUAVE_Samplers.py
@@ -105,6 +105,7 @@ def getVideo(file, start_frames, frame_count, index, file_name):
 
             if os.path.exists(path):
                 mouth_gray = cv2.imread(path)
+                mouth_gray = cv2.cvtColor(mouth_gray, cv2.COLOR_BGR2GRAY)
                 mouth_gray = cv2.resize(mouth_gray, (80, 60))
 
             else:
