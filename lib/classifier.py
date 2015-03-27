9c6413bc8cde4866a5f786246a9b2efcba78edaf
eisen.aviv@gmail.com <Nemesis23321>
2015-03-27 15:08:15 +0300

    Adding log

--- lib/classifier.py
+++ lib/classifier.py
@@ -29,7 +29,42 @@ import Regularizations
 import Optimizations
 import numpy
 
+def loadmat(filename):
+    '''
+    this function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    '''
+    data = scipy.io.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+def _check_keys(dict):
+    '''
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    '''
+    for key in dict:
+        if isinstance(dict[key], scipy.io.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict
+
+def _todict(matobj):
+    '''
+    A recursive function which constructs from matobjects nested dictionaries
+    '''
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, scipy.io.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
+
 class Classifier(object):
+
+
 
     @staticmethod
     def merge_gradients(gradients, layer):
@@ -68,8 +103,8 @@ class Classifier(object):
         test_gradient_path = sys.argv[2]
         layer = int(sys.argv[3])
 
-        train_gradients = scipy.io.loadmat(file(train_gradient_path, 'rb'))
-        test_gradients = scipy.io.loadmat(file(test_gradient_path, 'rb'))
+        train_gradients = loadmat(train_gradient_path)
+        test_gradients = loadmat(test_gradient_path)
 
         train_gradients = Classifier.merge_gradients(train_gradients, layer)
         test_gradients = Classifier.merge_gradients(test_gradients, layer)
