7af50215f9a7306af5b30995704fbd8e69508868
eisen.aviv@gmail.com <Nemesis23321>
2015-04-09 00:05:11 +0300

    sad

--- lib/classifier.py
+++ lib/classifier.py
@@ -168,7 +168,7 @@ class Classifier(object):
 
             samples.extend(sample.reshape((1, sample.shape[0])))
             if index % 10 == 9:
-                clf.partial_fit(samples, labels, 10)
+                clf.partial_fit(samples, labels, labels)
                 samples = []
                 gc.collect()
 
