e6085f44bf31752ad17e9d993dafd5209b126dcf
Aviv Eisenschtat <aviv@traxretail.com>
2015-04-10 19:57:33 +0300

    fix

--- lib/DataSetReaders/GUY_dataset.py
+++ lib/DataSetReaders/GUY_dataset.py
@@ -36,14 +36,13 @@ class GUYDataSet(DatasetBase):
         test_image_idx = idx_mat['tst_images_I']
         test_sen_idx = idx_mat['tst_sent_I']
 
-        self.trainset = [numpy.ndarray((CNN_output.shape[1], training_sen_idx.shape[0]), dtype=config.floatX),
+        self.trainset = [numpy.ndarray((CNN_output.shape[0], training_sen_idx.shape[0]), dtype=config.floatX),
                          numpy.ndarray((feature_vectors[0].shape[0], training_sen_idx.shape[0]), dtype=config.floatX)]
 
-        self.tuning = [numpy.ndarray((CNN_output.shape[1], validation_sen_idx.shape[0]), dtype=config.floatX),
+        self.tuning = [numpy.ndarray((CNN_output.shape[0], validation_sen_idx.shape[0]), dtype=config.floatX),
                        numpy.ndarray((feature_vectors[0].shape[0], validation_sen_idx.shape[0]), dtype=config.floatX)]
 
-
-        self.testset = [numpy.ndarray((CNN_output.shape[1], test_sen_idx.shape[0]), dtype=config.floatX),
+        self.testset = [numpy.ndarray((CNN_output.shape[0], test_sen_idx.shape[0]), dtype=config.floatX),
                         numpy.ndarray((feature_vectors[0].shape[0], test_sen_idx.shape[0]), dtype=config.floatX)]
 
 
