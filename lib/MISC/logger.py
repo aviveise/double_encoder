4db2217fe7464f7a5b97a2e768d9e4b9138d3536
Aviv Eisenschtat <aviv@traxretail.com>
2015-04-13 19:07:07 +0300

    chaning logger

--- lib/MISC/logger.py
+++ lib/MISC/logger.py
@@ -1,6 +1,7 @@
 __author__ = 'aviv'
 
 import datetime
+import os
 
 from singleton import Singleton
 
@@ -10,13 +11,17 @@ class OutputLog(object):
     __metaclass__= Singleton
 
     def __init__(self):
-        output_file_name = 'double_encoder_' + str(datetime.datetime.now()) + '.txt'
-        self.output_file = open(output_file_name, 'w+')
+        output_file_name = 'double_encoder_' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.txt'
+        self.path = os.getcwd()
+        self.output_file = open(os.path.join(self.path, output_file_name), 'w+')
 
     def write(self, message):
         print message
         self.output_file.write(message + '\n')
         self.output_file.flush()
+
+    def set_path(self, path):
+        self.path = path
 
     def __del__(self):
         self.output_file.close()
