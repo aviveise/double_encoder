fix in gradients

lib/double_encoder.py
@@ -183,9 +183,11 @@ class DoubleEncoder(object):
                 os.makedirs(train_dir_name)
 
             for ndx, gradient in enumerate(transformer.compute_outputs(data_set.trainset[0].T, data_set.trainset[1].T, 1)):
-                if sample and gradient.shape[0] > sample_number:
-                    indices = numpy.random.uniform(0, gradient.shape[0], sample_number)
-                    gradient = gradient[indices]
+                if sample:
+                    for param in gradient.keys():
+                        if gradient[param].shape[0] > sample_number:
+                            indices = numpy.random.uniform(0, gradient[param].shape[0], sample_number)
+                            gradient[param] = gradient[param][indices]
                 scipy.io.savemat(os.path.join(train_dir_name, "train_gradients_sample_{0}.mat".format(ndx)), gradient)
 
             test_dir_name = os.path.join(dir_name, 'test')
@@ -194,9 +196,11 @@ class DoubleEncoder(object):
                 os.makedirs(test_dir_name)
 
             for ndx, gradient in enumerate(transformer.compute_outputs(data_set.testset[0].T, data_set.testset[1].T, 1)):
-                if sample and gradient.shape[0] > sample_number:
-                    indices = numpy.random.uniform(0, gradient.shape[0], sample_number)
-                    gradient = gradient[indices]
+                if sample:
+                    for param in gradient.keys():
+                        if gradient[param].shape[0] > sample_number:
+                            indices = numpy.random.uniform(0, gradient[param].shape[0], sample_number)
+                            gradient[param] = gradient[param][indices]
                 scipy.io.savemat(os.path.join(train_dir_name, "test_gradients_sample_{0}.mat".format(ndx)), gradient)
 
         return stacked_double_encoder