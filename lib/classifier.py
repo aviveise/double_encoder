a94e8ca44647b9a9e7af98496b6dff24b7b2d929
Aviv Eisenschtat <aviv@traxretail.com>
2015-04-17 17:55:37 +0300

    fix

--- lib/classifier.py
+++ lib/classifier.py
@@ -234,29 +234,19 @@ class Classifier(object):
         else:
 
             x = None
-            train_labels = numpy.zeros(900)
-            test_labels = numpy.zeros(900)
-            for ndx_train, train_file in enumerate(gradient_train_files):
+            for train_file in enumerate(gradient_train_files):
                 fisher_vector = calc_gradient(train_file, layer)
-                if x is None:
-                    x = numpy.zeros((1800, fisher_vector.shape[0]))
-                x[ndx_train, :] = fisher_vector
                 file_name = os.path.split(os.path.splitext(train_file)[0])[1]
                 sample_number = int(file_name.split('_')[-1])
-                train_labels[ndx_train] = sample_number % 10
+                if x is None:
+                    x = numpy.zeros((1800, fisher_vector.shape[0]))
+                x[sample_number, :] = fisher_vector
 
             for ndx_test, test_file in enumerate(gradient_test_files):
                 fisher_vector = calc_gradient(test_file, layer)
-                x[ndx_test, :] = fisher_vector
                 file_name = os.path.split(os.path.splitext(test_file)[0])[1]
                 sample_number = int(file_name.split('_')[-1])
-                test_labels[ndx_test] = sample_number % 10
-
-            print 'train_labels'
-            print train_labels
-
-            print 'test_labels'
-            print test_labels
+                x[sample_number, :] = fisher_vector
 
             # for row_ndx, gradient_row_train_file in enumerate(gradient_train_files):
             #
@@ -315,13 +305,13 @@ class Classifier(object):
 
         svm_classifier = LinearSVC()
 
-        # train_labels = numpy.arange(10)
-        # for i in range(train_gradients.shape[0] / 10 - 1):
-        #    train_labels = numpy.concatenate((train_labels, numpy.arange(10)))
-        #
-        # test_labels = numpy.arange(10)
-        # for i in range(test_gradients.shape[0] / 10 - 1):
-        #    test_labels = numpy.concatenate((test_labels, numpy.arange(10)))
+        train_labels = numpy.arange(10)
+        for i in range(train_gradients.shape[0] / 10 - 1):
+           train_labels = numpy.concatenate((train_labels, numpy.arange(10)))
+
+        test_labels = numpy.arange(10)
+        for i in range(test_gradients.shape[0] / 10 - 1):
+           test_labels = numpy.concatenate((test_labels, numpy.arange(10)))
 
         svm_classifier.fit(train_gradients, train_labels)
 
