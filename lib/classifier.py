dd0c37f412227003afe64fcd34ae83d4ae66ee64
Aviv Eisenschtat <aviv@traxretail.com>
2015-04-17 17:43:41 +0300

    fx

--- lib/classifier.py
+++ lib/classifier.py
@@ -60,7 +60,7 @@ def compute_square(data_set, transformer):
 
 
 def lincompress(x):
-    U, S, V = scipy.linalg.svd(numpy.dot(x))
+    U, S, V = scipy.linalg.svd(numpy.dot(x, x.T))
     xc = numpy.dot(U, numpy.diag(numpy.sqrt(S))).T
 
     return xc
