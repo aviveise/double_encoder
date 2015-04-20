3815d6c69bb8afa8b9d5b7ed5cf222b5a39444fc
Aviv Eisenschtat <aviv@traxretail.com>
2015-04-20 21:24:21 +0300

    asd

--- lib/classifier.py
+++ lib/classifier.py
@@ -243,6 +243,7 @@ class Classifier(object):
                 fisher_vector = calc_gradient(train_file, layer)
                 fisher_vector -= numpy.mean(fisher_vector)
                 fisher_vector /= numpy.linalg.norm(fisher_vector)
+                fisher_vector = numpy.multiply(numpy.sqrt(numpy.abs(fisher_vector)), numpy.sign(fisher_vector))
                 file_name = os.path.split(os.path.splitext(train_file)[0])[1]
                 sample_number = int(file_name.split('_')[-1])
                 if x is None:
@@ -253,6 +254,7 @@ class Classifier(object):
                 fisher_vector = calc_gradient(test_file, layer)
                 fisher_vector -= numpy.mean(fisher_vector)
                 fisher_vector /= numpy.linalg.norm(fisher_vector)
+                fisher_vector = numpy.multiply(numpy.sqrt(numpy.abs(fisher_vector)), numpy.sign(fisher_vector))
                 file_name = os.path.split(os.path.splitext(test_file)[0])[1]
                 sample_number = int(file_name.split('_')[-1])
                 x[sample_number + 900, :] = fisher_vector
@@ -268,7 +270,7 @@ class Classifier(object):
         scipy.io.savemat(file_name, {'x': x})
 
         #pca = PCA(whiten=True, n_components=3000)
-        pca = FastICA(n_components=300)
+        #pca = FastICA(n_components=300)
 
         #print 'lincompres'
         #x = lincompress(x)
@@ -305,7 +307,7 @@ class Classifier(object):
         print 'test_gradient'
         print test_gradients
 
-        svm_classifier = LinearSVC()
+        svm_classifier = SVC()
 
         train_labels = numpy.arange(10)
         for i in range(train_gradients.shape[0] / 10 - 1):
