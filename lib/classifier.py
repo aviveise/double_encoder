b69422cb48a828157afb505e4cfca6242ece8a19
Aviv Eisenschtat <aviv@traxretail.com>
2015-04-20 23:25:44 +0300

    asd

--- lib/classifier.py
+++ lib/classifier.py
@@ -269,7 +269,7 @@ class Classifier(object):
         #exporting matrix
         scipy.io.savemat(file_name, {'x': x})
 
-        pca = PCA(whiten=True, n_components=3000)
+        #pca = PCA(whiten=True, n_components=3000)
         #pca = FastICA(n_components=300)
 
         #print 'lincompres'
@@ -287,10 +287,10 @@ class Classifier(object):
 
 
         #print 'pca'
-        pca.fit(train_gradients)
+        #pca.fit(train_gradients)
 
-        train_gradients = pca.transform(train_gradients)
-        test_gradients = pca.transform(test_gradients)
+        #train_gradients = pca.transform(train_gradients)
+        #test_gradients = pca.transform(test_gradients)
 
         # train_gradients /= numpy.dot(numpy.linalg.norm(train_gradients, axis=1).reshape((train_gradients.shape[0], 1)),
         #                              numpy.ones((1, train_gradients.shape[1])))
@@ -307,7 +307,7 @@ class Classifier(object):
         print 'test_gradient'
         print test_gradients
 
-        svm_classifier = LinearSVC(C=0.05)
+        svm_classifier = LinearSVC()
 
         train_labels = numpy.arange(10)
         for i in range(train_gradients.shape[0] / 10 - 1):
