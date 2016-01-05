f6c1e2a20300ab2cd764154aa5ad2efdecbdccc0
Aviv Eisenschtat <aviv@traxretail.com>
2016-01-05 23:09:02 +0200

    removing cv2

--- lib/DataSetReaders/__init__.py
+++ lib/DataSetReaders/__init__.py
@@ -6,8 +6,8 @@ from XRBM_dataset import XRBMDataSet
 from XRBM_dataset import XRBMSampledDataSet
 from CARS_dataset import CARSDataSetTar
 from DRUGS_dataset import DRUGSDataSet
-from JAPAN_dataset import JAPANDataSet
-from JAPAN_dataset import JAPANDataSetVideo
+# from JAPAN_dataset import JAPANDataSet
+# from JAPAN_dataset import JAPANDataSetVideo
 from FISH_dataset import FISHDataSet
 from RAND_dataset import RANDDataSet
 from GUY_dataset import GUYDataSet
