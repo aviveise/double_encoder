63094331858136c6ae23de26f2b7c123ec1e4a1b
Aviv Eisenschtat <aviv@traxretail.com>
2016-01-27 00:05:15 +0200

    fix rank

--- lib/MISC/utils.py
+++ lib/MISC/utils.py
@@ -558,8 +558,7 @@ def complete_rank(x, y, reduce_x=0, normalize_axis=1):
 
         if not x.shape[0] % reduce_x == 0:
             for i in range(0, reduce_x - x.shape[0] % reduce_x):
-                x = numpy.vstack((x, x[-1,:]))
-                y = numpy.vstack((y, y[-1,:]))
+                x_n = numpy.vstack((x_n, x_n[-1,:]))
 
         num_X_samples = x.shape[0]
         num_Y_samples = y.shape[0]
