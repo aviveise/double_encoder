d85387ea8a482a86ba291cefeae7c7080da16b89
Aviv Eisenschtat <aviv@traxretail.com>
2016-01-30 19:16:03 +0200

    adding normalization

--- lib/DataSetReaders/dataset_base.py
+++ lib/DataSetReaders/dataset_base.py
@@ -9,7 +9,7 @@ import numpy
 import pickle
 
 from sklearn.decomposition import PCA
-from lib.MISC.utils import scale_cols
+from lib.MISC.utils import scale_cols, normalize
 from lib.MISC.logger import OutputLog
 
 __author__ = 'aviv'
@@ -32,6 +32,7 @@ class DatasetBase(object):
         scale_rows = bool(int(data_set_parameters['scale_samples']))
         whiten = bool(int(data_set_parameters['whiten']))
         pca = map(int, data_set_parameters['pca'].split())
+        normalize_data = map(int, data_set_parameters['normalize'].split())
 
         path = self.dataset_path
         if not os.path.isdir(self.dataset_path):
@@ -40,6 +41,7 @@ class DatasetBase(object):
         train_file = os.path.join(path, 'train.p')
         test_file = os.path.join(path, 'test.p')
         validate_file = os.path.join(path, 'validate.p')
+        params_file = os.path.join(path, 'params.p')
 
         if os.path.exists(train_file) and \
                 os.path.exists(test_file) and \
@@ -50,6 +52,11 @@ class DatasetBase(object):
                 self.testset = hickle.load(f)
             with open(validate_file, 'r') as f:
                 self.tuning = hickle.load(f)
+            if os.path.exists(params_file):
+                with open(params_file, 'r') as f:
+                    loaded_data_set_parameters = hickle.load(f)
+            else:
+                    loaded_data_set_parameters = 'No params file found'
 
             self.trainset = self.trainset[0].astype(dtype=theano.config.floatX), self.trainset[1].astype(
                 dtype=theano.config.floatX)
@@ -62,9 +69,29 @@ class DatasetBase(object):
             OutputLog().write('Training set size = %d' % self.trainset[0].shape[0])
             OutputLog().write('Test set size = %d' % self.testset[0].shape[0])
 
+            OutputLog().write('Dataset params: {0}'.format(loaded_data_set_parameters))
+
             return
 
         self.build_dataset()
+
+        if normalize_data[0]:
+            train_set_x, normalizer_x = normalize(self.trainset[0])
+            self.trainset = train_set_x, train_set_y
+            self.tuning = (normalizer_x.transform(self.tuning[0]), self.tuning[1])
+            self.testset = (normalizer_x.transform(self.testset[0]), self.testset[1])
+            scaler_x_path = os.path.join(path, 'scaler_x.p')
+
+            pickle.dump(normalizer_x, file(scaler_x_path, 'w'))
+
+        if normalize_data[0]:
+            train_set_y, normalizer_y = normalize(self.trainset[1])
+            self.trainset = train_set_x, train_set_y
+            self.tuning = (self.tuning[0], normalizer_y.transform(self.tuning[1]))
+            self.testset = (self.testset[0], normalizer_y.transform(self.testset[1]))
+            scaler_y_path = os.path.join(path, 'scaler_y.p')
+
+            pickle.dump(normalizer_y, file(scaler_y_path, 'w'))
 
         if scale[0]:
             train_set_x, scaler_x = scale_cols(self.trainset[0])
@@ -121,10 +148,13 @@ class DatasetBase(object):
         hickle.dump(self.trainset, file(train_file, 'w'))
         hickle.dump(self.testset, file(test_file, 'w'))
         hickle.dump(self.tuning, file(validate_file, 'w'))
+        hickle.dump(data_set_parameters, file(params_file, 'w'))
 
         OutputLog().write('Dataset dimensions = %d, %d' % (self.trainset[0].shape[1], self.trainset[1].shape[1]))
         OutputLog().write('Training set size = %d' % self.trainset[0].shape[0])
         OutputLog().write('Test set size = %d' % self.testset[0].shape[0])
+
+        OutputLog().write('Dataset params: {0}'.format(data_set_parameters))
 
         if self.tuning is not None:
             OutputLog().write('Tuning set size = %d' % self.tuning[0].shape[0])
